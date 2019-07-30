ac4cb95bb5c82feb9c84a24640879aabb8c9704a
JhonatanL <jhonatandef17@gmail.com>
2019-07-30 17:14:37 -0500

    Updates

---
+++ precache-manifest.5d753eb63b82272b7f304263386589e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8419fd03b105992c843abc25b509c4f4",
+    "url": "/moderna/index.html"
+  },
+  {
+    "revision": "75c9567545f94647450b",
+    "url": "/moderna/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/moderna/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "75c9567545f94647450b",
+    "url": "/moderna/static/js/main.992930ee.chunk.js"
+  },
+  {
+    "revision": "e0e02375b7051809341e",
+    "url": "/moderna/static/js/runtime~main.e804d941.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moderna/static/media/logo.5d5d9eef.svg"
+  }
+]);
